f5554fcedd280f7bef8ee8dd408e808ae1d24024
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-05-13 15:45:28 -0400

    added loop every 30 minutes to check for ip change and update dns record if changed

--- dyndns.rb
+++ dyndns.rb
@@ -4,18 +4,18 @@ require 'dotenv/load'
 
 token = ENV['CLOUDFLARE_TOKEN']
 
-ip = RestClient.get('https://api.ipify.org')
-
-Cloudflare.connect(token: token) do |connection|	# Get all available zones:
-    zones = connection.zones
-    # puts zones
-    zone = connection.zones.find_by_name("kyletomanelli.com")
-    # puts zone
-    dns_records = zone.dns_records
-    str = dns_records.first.to_s.split(' ').last
-    puts str
-    # dns_records.each do |i|
-    #     puts i 
-    # end
+loop do
+    ip = RestClient.get('https://api.ipify.org')
+    
+    Cloudflare.connect(token: token) do |connection|
+        zone = connection.zones.find_by_name("kyletomanelli.com")
+        record = zone.dns_records.find_by_name("rdp.kyletomanelli.com")
+    
+        if record.content != ip
+            record.delete
+            zone.dns_records.create('A', 'rdp', ip, proxied: false)
+        end 
+        puts 'fuck yeah'
+    end
+    sleep(1800)
 end
-
